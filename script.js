0a1b0de86751f0ec9ba2f351734637541a832f3f
Matt D'Souza <matt.dsouza@gmail.com>
2016-01-03 17:15:53 -0500

    Refactored everything, resized info div

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(function() {
-	$(".hover-effect").click(function(event) {
+	$(".project-icon").click(function(event) {
 
 		var clicked = $(this).attr("id");
 		$(".current-project").removeClass("current-project");
 		$("#" + clicked + ".project-info").addClass("current-project");
+		$("#project-info-list").animate({"height":$("#"+clicked+".project-info").height()},200);
 	});
 });
