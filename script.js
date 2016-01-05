03d93d8dab5bdedb1bc8de7ee0c0eb133ead6b30
Matt D'Souza <matt.dsouza@gmail.com>
2016-01-04 21:03:39 -0500

    dynamic background colours

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ $(function() {
 	$(".project-icon").click(function(event) {
 
 		var clicked = $(this).attr("id");
+		var color = $(this).css("background-color");
+		console.log(color);
 		$(".current-project").removeClass("current-project");
 		$("#" + clicked + ".project-info").addClass("current-project");
 		$("#project-info-list").animate({"height":$("#"+clicked+".project-info").height()},200);
+		$("#projects").animate({backgroundColor:color},150);
+
 	});
 });
