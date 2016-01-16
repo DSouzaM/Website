ba0a7375bf760ea2edceecbcbcdb6533b6a0de44
Matt D'Souza <matt.dsouza@gmail.com>
2016-01-16 02:52:22 -0500

    major rehaul. simple interface

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ $(function() {
 	$(".project-icon").click(function(event) {
 
 		var clicked = $(this).attr("id");
-		var color = $(this).css("background-color");
-		console.log(color);
-		$(".current-project").removeClass("current-project");
-		$("#" + clicked + ".project-info").addClass("current-project");
-		$("#project-info-list").animate({"height":$("#"+clicked+".project-info").height()},200);
-		$("#projects").animate({backgroundColor:color},150);
 
+		$("#" + clicked + ".modal").modal('toggle');
 	});
 });
