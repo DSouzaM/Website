64a64d0f7a21c8292a878928054379cab5996786
Matt D'Souza <matt.dsouza@gmail.com>
2016-09-17 20:17:37 -0400

    add experiences

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(function() {
-	$(".project-icon").click(function(event) {
+	$(".item").click(function(event) {
 
 		var clicked = $(this).attr("id");
-
-		$("#" + clicked + "-modal.modal").modal('toggle');
+        var $modal = $("#" + clicked + "-modal.modal");
+        if ($modal.length !== 0) {
+		  $("#" + clicked + "-modal.modal").modal("toggle");
+        }
 	});
 });
